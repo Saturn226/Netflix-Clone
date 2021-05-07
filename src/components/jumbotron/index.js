4abbde1815a90112a5cd2534adf33b9ed1386a54
Saturn226 <serenehopes@gmail.com>
2021-05-07 11:26:27 -0400

    styled jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import {Inner, Container, Title, SubTitle, Image, Pane } from './styles/jumbotron'
+export default function Jumbotron({children, direction="row", ...restProps}){
+    return(
+        <Inner direction={direction}>
+            {children}
+        </Inner>
+    )
+} 
 
-export default function Jumbotron(){
-    return(<p>I am the Jumbotron</p>)
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}){
+    return <Container {...restProps}>{children}</Container>
+} 
+
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}){
+    return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}){
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({children, ...restProps}){
+    return <Image {...restProps}/>
+}
+
+Jumbotron.Pane = function JumbotronPane({children, ...restProps}){
+    return <Pane {...restProps}>{children}</Pane>
 }
